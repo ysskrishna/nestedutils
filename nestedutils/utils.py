5855b10eb79cc52841a1db555fc0b55f685c7700
Siva Sai <26503640+ysskrishna@users.noreply.github.com>
2025-12-20 10:19:11 +0530

    Siva | delete unused files

--- nestedutils/utils.py
+++ nestedutils/utils.py
@@ -1,22 +1,3 @@
-def escape_key(key: str, escape: str = "\\", sep: str = ".") -> str:
-    """Escape separators inside keys."""
-    return key.replace(escape, escape + escape).replace(sep, escape + sep)
-
-
-def unescape_key(key: str, escape: str = "\\", sep: str = ".") -> str:
-    """Unescape separators."""
-    i = 0
-    out = []
-    while i < len(key):
-        if key[i] == escape:
-            i += 1
-            if i < len(key):
-                out.append(key[i])
-        else:
-            out.append(key[i])
-        i += 1
-    return "".join(out)
-
 
 def validate_path(path: str) -> bool:
     """Make sure path is syntactically valid."""
